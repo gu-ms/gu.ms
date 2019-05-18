81201f3401c83647350dca1e7ad407e582b8e25e
Aditya Naga Sanjeevi, Yellapu <sanjeev.any@gmail.com>
2019-05-18 23:56:10 +0530

    added additional handlers for testing

--- osvn.go
+++ osvn.go
@@ -3,14 +3,37 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+	"math/rand"
 )
 
 func main() {
-	http.HandleFunc("/myip", handler)
-	http.HandleFunc("/whatismyip", handler)
+	http.HandleFunc("/myip", ip)
+	http.HandleFunc("/whatismyip", ip)
+	http.HandleFunc("/time", _time)
+	http.HandleFunc("/pass", password)
 	http.ListenAndServe(":9999", nil)
 }
 
-func handler(write http.ResponseWriter, read *http.Request) {
+func ip(write http.ResponseWriter, read *http.Request) {
 	fmt.Fprintf(write, read.RemoteAddr)
+}
+
+func _time(write http.ResponseWriter, read *http.Request) {
+	t := time.Now()
+	fmt.Fprintf(write, t.Format("13:53:35"))
+}
+
+func password(write http.ResponseWriter, read *http.Request) {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~`!@#$%^&*()_+-=<>?\",./':;{}[]|\\")
+	b := make([]rune, 16)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+
+	fmt.Fprintf(write, string(b))
+}
+
+func init() {
+    rand.Seed(time.Now().UnixNano())
 }
